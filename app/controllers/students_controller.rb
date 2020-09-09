5d6515ef8a6d943418f86cb2a41a8751d904b540
Debora Ermel <debbyermel@gmail.com>
2020-09-08 17:44:32 -0700

    white list

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -8,4 +8,19 @@ class StudentsController < ApplicationController
     @student = Student.new
   end
 
+  def create
+     @student = Student.new(student_params)
+     if @student.save
+      flash[:success] = 'You are in!!'
+      redirect_to root_path
+     else
+      render 'new'
+      flash[:notice] = 'You are in!!'
+     end
+  end
+
+  private
+    def student_params
+      params.require(:student).permit(:name, :email)
+    end
 end
