52c625b439e26381753772eff3d7205dd3a860a7
Debora Ermel <debbyermel@gmail.com>
2020-09-09 18:12:54 -0700

    Add secure password digest

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_09_08_232845) do
+ActiveRecord::Schema.define(version: 2020_09_10_010151) do
 
   create_table "courses", force: :cascade do |t|
     t.string "short_name"
@@ -25,6 +25,7 @@ ActiveRecord::Schema.define(version: 2020_09_08_232845) do
     t.string "email"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "password_digest"
   end
 
 end
