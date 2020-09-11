fdc90c4a871ba414670801fdfa1aa1362880153e
Debora Ermel <debbyermel@gmail.com>
2020-09-10 17:40:28 -0700

    Crud login

--- app/controllers/login_controller.rb
+++ app/controllers/login_controller.rb
@@ -3,9 +3,21 @@ class LoginController < ApplicationController
   end
 
   def create
+    student = Student.find_by(email: params[:login][:email].downcase)
+    if student && Student.authenticate(params[:login] [:password])
+      session[:student_id] = student.id
+      flash[:notice] = 'You have successfully logged in.'
+      redirect_to student
+    else
+      flash.now[:notice] = 'Something was wrong with your login.'
+      render 'new'
+    end
   end
 
   def destroy
+    session[:student_id] = nil
+    flash[:notice] = 'Bye, successfully logged out'
+    redirect_to root_path
   end
 
 end
